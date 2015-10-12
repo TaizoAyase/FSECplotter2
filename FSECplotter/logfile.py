d9a6e1b8374bad99c86244014c924b2058388392
Taizo_Ayase <zxcvbnmkind@gmail.com>
2015-10-13 02:35:10 +0900

    Implemented Plotter core methods
    using matplotlib.pyplot.figure, enables OOP

--- FSECplotter/logfile.py
+++ FSECplotter/logfile.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
@@ -7,16 +6,19 @@ import numpy as np
 import codecs
 import re
 import os
-from section import Section
+from .section import Section
 
 class LogFile:
   def __init__(self):
     self.sections = []
     self.file_name = None
+    self.flow_rate = None
+    self.no_of_detectors = None
 
   def parse(self, filename):
     self.__parse_logfile(filename)
-    self.file_name, ext = os.path.basename(filename)
+    filename, ext = os.path.splitext( os.path.basename(filename) )
+    self.file_name = filename
     return self
 
   def append_section(self, section):
@@ -24,12 +26,41 @@ class LogFile:
     self.sections.append(section)
     return self.sections
 
+  # return section that match the RE of section_name
   def find_section(self, section_name):
+    # first, replace "()" to "\(\)"
     secname = self.__sub_parenthesis(section_name)
     pat = re.compile(secname)
-    ary_tmp = [x.name() for x in self.sections]
-    index = [i for i, name in enumerate(ary_tmp) if pat.search(name)]
-    return int(index[0])
+    name_ary = [x.name() for x in self.sections]
+    index = [i for i, name in enumerate(name_ary) if pat.search(name)]
+    if len(index) > 1:
+      raise
+    return self.sections[index[0]]
+
+  # get flow rate from the file name of method file 
+  # if cannot, raise Error
+  def flowrate(self):
+    # if already set, end...
+    if self.flow_rate:
+      return self.flow_rate
+
+    methodfiles_ary= self.__get_params_ary("Original Files", "Method File")
+
+    # search float in file name
+    pat = re.compile(r"\d\.\d+")
+    matched_str = pat.findall(methodfiles_ary[0])
+    if matched_str:
+      self.flow_rate = float(matched_str[0])
+      return self.flow_rate
+    else:
+      raise NoMatchedFlowRateError
+
+  # returns # of detectors and set it to self.no_of_detectors
+  def num_of_detector(self):
+    num_detectors_ary = self.__get_params_ary("Configuration", "# of Detectors")
+    self.no_of_detectors = int(num_detectors_ary[0])
+    return self.no_of_detectors
+    
 
   ### private methods
 
@@ -58,5 +89,15 @@ class LogFile:
     tmp = re.sub(r"\(", "\(", section_name)
     return re.sub(r"\)", "\)", tmp)
 
+  def __get_params_ary(self, section_name, param_name):
+    params_ary = self.find_section(section_name).params()
+    for par in params_ary:
+      if param_name in par:
+        rm_name_ary = par.copy()
+        rm_name_ary.pop(0)
+        return rm_name_ary
 
 
+### exception
+class NoMatchedFlowRateError(Exception):
+  pass
--- FSECplotter/plotter.py
+++ FSECplotter/plotter.py
@@ -1,6 +1,53 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from __future__ import print_function
-import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+from .logfile import LogFile
+
+class Plotter():
+  def __init__(self, logfiles, sec_name):
+    self.sec_to_plot = sec_name
+
+    # setup plot env.
+    self.__fig = figure()
+    self.__axes = self.__fig.add_subplot(111)
+    self.__axes.hold(True) # superimporse the all plots
+
+    # get the information of logfiles
+    self.__logfiles = [ LogFile().parse(f) for f in logfiles ]
+    self.__data = [ log.find_section(sec_name).data() for log in self.__logfiles ]
+    self.__filenames = [ log.file_name for log in self.__logfiles ]
+
+  def make_plot(self):
+    # at first, set flow rate for each logfile
+    [log.flowrate() for log in self.__logfiles]
+
+    for (log, df) in zip(self.__logfiles, self.__data):
+      self.__axes.plot(df[:, 0] * log.flow_rate, df[:, 1])
+
+    self.__axes.legend(self.__filenames)
+    self.__axes.set_xlabel("Volume(ml)")
+    self.__axes.set_ylabel("FL intensity")
+
+  def show_plot(self):
+    pass
+
+  def save_plot(self, save_name):
+    self.__fig.savefig(save_name)
+    return None
+
+  ### private methods
 
 
+if __name__ == '__main__':
+  from glob import glob
+
+  files = glob("./test/fixture/test*.txt")
+
+  detector = "B"
+  channel_no = 2
+  sec_name = "LC Chromatogram(Detector %s-Ch%d)" % (detector, channel_no)
+  plt = Plotter(files, sec_name)
+  plt.make_plot()
+  plt.save_plot("test.png")
