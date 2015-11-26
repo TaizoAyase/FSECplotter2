50850dcac51caafc4dce037a70758aeac962398a
Taizo_Ayase <zxcvbnmkind@gmail.com>
2015-11-26 23:55:43 +0900

    Migrate Logfileview to LogfileListWidget
    the LogfileView has independent role,
    so the list and open/remove/move up/move down buttons are
    moved to LogfileListWidget

--- FSECplotter/pyqt/logfilelist.py
+++ FSECplotter/pyqt/logfilelist.py
@@ -160,14 +160,107 @@ class LogfileListView(QtWidgets.QTreeView):
       event.ignore()
 
 
+class LogfileListWidget(QtWidgets.QWidget):
+  """Widget for control the logfiles"""
+  def __init__(self, parent = None):
+    super(LogfileListWidget, self).__init__(parent)
+
+    # Standard item model
+    self.model = LogfileModel(0, 5, self)
+    self.selection_model = QtCore.QItemSelectionModel(self.model)
+
+    self.treeview = LogfileListView(self)
+
+    # set ItemModel to treeview widget
+    self.treeview.setModel(self.model)
+    self.treeview.setSelectionModel(self.selection_model)
+
+    # TreeView setting
+    #self.treeview.setColumnWidth(1, 140)
+    self.treeview.setSelectionMode(
+      QtWidgets.QAbstractItemView.ExtendedSelection
+    )
+    self.selection_model.clear()
+
+    # set buttons
+    self.open_button = QtWidgets.QPushButton(self)
+    self.open_button.setObjectName("Open button")
+    self.open_button.setText("Open file")
+
+    self.delete_button = QtWidgets.QPushButton(self)
+    self.delete_button.setObjectName("Delete button")
+    self.delete_button.setText("Remove file")
+
+    self.move_up_button = QtWidgets.QPushButton(self)
+    self.move_up_button.setObjectName("Move-up button")
+    self.move_up_button.setText("Move up")
+    self.move_down_button = QtWidgets.QPushButton(self)
+    self.move_down_button.setObjectName("Move-down button")
+    self.move_down_button.setText("Move down")
+
+    self.gridLay1 = QtWidgets.QGridLayout()
+    self.gridLay1.addWidget(self.open_button, 0, 0, 1, 1)
+    self.gridLay1.addWidget(self.delete_button, 0, 1, 1, 1)
+    self.gridLay1.addWidget(self.move_up_button, 1, 0, 1, 1)
+    self.gridLay1.addWidget(self.move_down_button, 1, 1, 1, 1)
+
+    self.verLay1 = QtWidgets.QVBoxLayout()
+    self.verLay1.addWidget(self.treeview)
+
+    self.verLay2 = QtWidgets.QVBoxLayout(self)
+    self.verLay2.addLayout(self.verLay1)
+    self.verLay2.addLayout(self.gridLay1)
+
+    # signal slot definition
+    self.open_button.clicked.connect(self.open_file)
+    self.delete_button.clicked.connect(self.delete_file)
+    self.move_up_button.clicked.connect(lambda: self.move_selected(-1))
+    self.move_down_button.clicked.connect(lambda: self.move_selected(1))
+
+  def open_file(self):
+    # open the file selection dialog
+    # enable multiple selection
+    filename = QtWidgets.QFileDialog.getOpenFileNames(
+      self, "Open file", os.path.expanduser('~'))
+    for f in filename[0]:
+      self.model.add_item(f)
+
+  def delete_file(self):
+    # TODO: check this implementation
+    # this method for get index is correct?
+    current_index = self.selection_model.currentIndex()
+    current_row = current_index.row()
+    self.model.delete_item(current_row)
+    self.selection_model.clear()
+
+  def move_selected(self, shift):
+    current_index = self.selection_model.currentIndex()
+    current_row = current_index.row()
+    if current_row == -1:
+      QtWidgets.QApplication.beep()
+      return
+
+    moved_to = self.model.move_item(current_row, int(shift))
+
+    # set selection to moved row
+    self.selection_model.clear()
+
+    left_idx = self.model.index(moved_to, 0, QtCore.QModelIndex())
+    right_idx = self.model.index(moved_to, self.model.columnCount() - 1)
+    row_selection = QtCore.QItemSelection()
+    row_selection.select(left_idx, right_idx)
+
+    self.selection_model.setCurrentIndex(left_idx, QtCore.QItemSelectionModel.Rows)
+    self.selection_model.select(row_selection, QtCore.QItemSelectionModel.Select)
+
 
 if __name__ == '__main__':
   import sys
   app = QtWidgets.QApplication(sys.argv)
   
-  win = LogfileListView()
+  win = LogfileListWidget()
   model = LogfileModel(0, 5, win)
-  win.setModel(model)
+  #win.setModel(model)
 
   win.show()
 
--- FSECplotter/pyqt/mainwindow.py
+++ FSECplotter/pyqt/mainwindow.py
@@ -16,70 +16,26 @@ class MainWindow(QtWidgets.QMainWindow):
     #layout = QtWidgets.QVBoxLayout(self)
     self.centralWidget = QtWidgets.QWidget(self)
 
-    # Standard item model
-    self.model = LogfileModel(0, 5, self)
-    self.selection_model = QtCore.QItemSelectionModel(self.model)
-
     # set list view and plot widgets
-    self.treeview = LogfileListView(self.centralWidget)
+    self.treeview = LogfileListWidget(self.centralWidget)
     self.plotarea = PlotArea(self.centralWidget)   
 
-    # set ItemModel to treeview widget
-    self.treeview.setModel(self.model)
-    self.treeview.setSelectionModel(self.selection_model)
-
     # make first plot
-    data = self.model.get_current_data()
+    data = self.treeview.model.get_current_data()
     self.plotarea.plot_fig(data)
-
-    # TreeView setting
-    #self.treeview.setColumnWidth(1, 140)
-    self.treeview.setSelectionMode(
-      QtWidgets.QAbstractItemView.ExtendedSelection
-    )
 
     # set buttons
     self.redraw_button = QtWidgets.QPushButton(self.centralWidget)
     self.redraw_button.setObjectName("Redraw button")
     self.redraw_button.setText("Redraw")
 
-    self.open_button = QtWidgets.QPushButton(self.centralWidget)
-    self.open_button.setObjectName("Open button")
-    self.open_button.setText("Open file")
-
-    self.delete_button = QtWidgets.QPushButton(self.centralWidget)
-    self.delete_button.setObjectName("Delete button")
-    self.delete_button.setText("Remove file")
-
-    self.move_up_button = QtWidgets.QPushButton(self.centralWidget)
-    self.move_up_button.setObjectName("Move-up button")
-    self.move_up_button.setText("Move up")
-    self.move_down_button = QtWidgets.QPushButton(self.centralWidget)
-    self.move_down_button.setObjectName("Move-down button")
-    self.move_down_button.setText("Move down")
-
     self.savefig_button = QtWidgets.QPushButton(self.centralWidget)
     self.savefig_button.setObjectName("Save Fig. button")
     self.savefig_button.setText("Save Fig.")
 
     # signal slot definition
-    self.redraw_button.clicked.connect(self.redraw)
-    self.open_button.clicked.connect(self.open_file)
-    self.delete_button.clicked.connect(self.delete_file)
-    self.move_up_button.clicked.connect(lambda: self.move_selected(-1))
-    self.move_down_button.clicked.connect(lambda: self.move_selected(1))
-    self.savefig_button.clicked.connect(self.save_figure)
-
-    # left-hand layout
-    self.gridLay1 = QtWidgets.QGridLayout()
-    self.gridLay1.addWidget(self.open_button, 0, 0, 1, 1)
-    self.gridLay1.addWidget(self.delete_button, 0, 1, 1, 1)
-    self.gridLay1.addWidget(self.move_up_button, 1, 0, 1, 1)
-    self.gridLay1.addWidget(self.move_down_button, 1, 1, 1, 1)
-
-    self.verLay1 = QtWidgets.QVBoxLayout()
-    self.verLay1.addWidget(self.treeview)
-    self.verLay1.addLayout(self.gridLay1)
+    #self.redraw_button.clicked.connect(self.redraw)
+    #self.savefig_button.clicked.connect(self.save_figure)
 
     # right-hand layout
     self.gridLay2 = QtWidgets.QGridLayout()
@@ -91,7 +47,7 @@ class MainWindow(QtWidgets.QMainWindow):
     self.verLay2.addLayout(self.gridLay2)
 
     self.horizontalLayout = QtWidgets.QHBoxLayout(self.centralWidget)
-    self.horizontalLayout.addLayout(self.verLay1)
+    self.horizontalLayout.addWidget(self.treeview)
     self.horizontalLayout.addLayout(self.verLay2)
 
     self.setCentralWidget(self.centralWidget)
@@ -109,41 +65,6 @@ class MainWindow(QtWidgets.QMainWindow):
         QtWidgets.QMessageBox.Ok)
       return
     self.plotarea.plot_fig(data)
-
-  def open_file(self):
-    # open the file selection dialog
-    # enable multiple selection
-    filename = QtWidgets.QFileDialog.getOpenFileNames(
-      self, "Open file", os.path.expanduser('~'))
-    for f in filename[0]:
-      self.model.add_item(f)
-
-  def delete_file(self):
-    # TODO: check this implementation
-    # this method for get index is correct?
-    current_index = self.selection_model.currentIndex()
-    current_row = current_index.row()
-    self.model.delete_item(current_row)
-
-  def move_selected(self, shift):
-    current_index = self.selection_model.currentIndex()
-    current_row = current_index.row()
-    if current_row == -1:
-      QtWidgets.QApplication.beep()
-      return
-
-    moved_to = self.model.move_item(current_row, int(shift))
-
-    # set selection to moved row
-    self.selection_model.clear()
-
-    left_idx = self.model.index(moved_to, 0, QtCore.QModelIndex())
-    right_idx = self.model.index(moved_to, self.model.columnCount() - 1)
-    row_selection = QtCore.QItemSelection()
-    row_selection.select(left_idx, right_idx)
-
-    self.selection_model.setCurrentIndex(left_idx, QtCore.QItemSelectionModel.Rows)
-    self.selection_model.select(row_selection, QtCore.QItemSelectionModel.Select)
 
   def save_figure(self):
     pass
