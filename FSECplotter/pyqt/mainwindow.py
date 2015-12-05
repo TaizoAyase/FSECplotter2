97591f8465cbfdaa88f04c65f3c4df3317c85456
Taizo_Ayase <zxcvbnmkind@gmail.com>
2015-12-05 16:44:06 +0900

    Add file and tool menu
    tool menu content is not implemented

--- FSECplotter/pyqt/mainwindow.py
+++ FSECplotter/pyqt/mainwindow.py
@@ -29,6 +29,55 @@ class MainWindow(QtWidgets.QMainWindow):
         self.resize(1200, 600)
         self.setWindowTitle("FSEC plotter 2")
 
+        self.createActions()
+        self.createMenus()
+
+    def createActions(self):
+        # open
+        self.openAction = QtWidgets.QAction("Open", self)
+        self.openAction.setShortcut("Ctrl+O")
+        self.openAction.setStatusTip("Open the new log file.")
+        self.openAction.triggered.connect(self.treeview.open_file)
+
+        # save figure
+        self.saveAsAction = QtWidgets.QAction("Save fig as ...", self)
+        self.saveAsAction.setShortcut("Ctrl+Shift+S")
+        self.saveAsAction.setStatusTip("Save current figure as new name.")
+        self.saveAsAction.triggered.connect(self.plotarea.save_figure)
+
+        # quick save fig
+        self.quickSaveAction = QtWidgets.QAction("Quick save", self)
+        self.quickSaveAction.setShortcut("Ctrl+S")
+        self.quickSaveAction.setStatusTip("One click save.")
+        self.quickSaveAction.triggered.connect(self.plotarea.quick_save_figure)
+
+        # app quit
+        self.quitAction = QtWidgets.QAction("Quit", self)
+        self.quitAction.setShortcut("Ctrl+Q")
+        self.quitAction.setStatusTip("Quit app.")
+        self.quitAction.triggered.connect(QtWidgets.QApplication.closeAllWindows)
+
+        # tools
+        # fsec-ts
+        self.tsAction = QtWidgets.QAction("calc Tm", self)
+        self.tsAction.setStatusTip("Calc Tm from FSEC-TS data.")
+        self.tsAction.triggered.connect(self.fsec_ts)
+
+    def createMenus(self):
+        # file menu
+        self.fileMenu = self.menuBar().addMenu("File")
+        self.fileMenu.addAction(self.openAction)
+        self.fileMenu.addAction(self.saveAsAction)
+        self.fileMenu.addAction(self.quickSaveAction)
+        self.fileMenu.addSeparator()
+        self.fileMenu.addAction(self.quitAction)
+
+        # tool menu
+        self.toolMenu = self.menuBar().addMenu("Tool")
+        self.toolMenu.addAction(self.tsAction)
+
+    def fsec_ts(self):
+        pass
 
 if __name__ == '__main__':
     import sys
