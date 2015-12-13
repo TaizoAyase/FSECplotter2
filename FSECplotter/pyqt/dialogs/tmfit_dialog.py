7a0d06c8d4f7bd283fb605003b864cfb11b55a82
Taizo_Ayase <zxcvbnmkind@gmail.com>
2015-12-13 18:31:38 +0900

    change layout of fitTm dialog

--- FSECplotter/pyqt/dialogs/tmfit_dialog.py
+++ FSECplotter/pyqt/dialogs/tmfit_dialog.py
@@ -10,6 +10,7 @@ import matplotlib
 matplotlib.use("Qt5Agg")
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
+import os
 
 class TmFitDialog(QtWidgets.QDialog):
 
@@ -21,20 +22,46 @@ class TmFitDialog(QtWidgets.QDialog):
         self.canvas = FigureCanvas(self.fig)
         self.axes.set_xlabel("Temperature [C]")
         self.axes.set_ylabel("Relative Intensity")
+        self.axes.grid()
 
-        layout = QtWidgets.QHBoxLayout()
+        self.hori_layout = QtWidgets.QHBoxLayout()
+        spacerItem = QtWidgets.QSpacerItem(40, 20,
+            QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.save_fig_button = QtWidgets.QPushButton()
+        self.save_fig_button.setText("Save fitting curve")
+        self.ok_button = QtWidgets.QPushButton()
+        self.ok_button.setText("OK")
+        self.hori_layout.addItem(spacerItem)
+        self.hori_layout.addWidget(self.save_fig_button)
+        self.hori_layout.addWidget(self.ok_button)
+
+        layout = QtWidgets.QVBoxLayout(self)
         layout.addWidget(self.canvas)
+        layout.addLayout(self.hori_layout)
         self.setLayout(layout)
+
+        self.ok_button.clicked.connect(self.accept)
+        self.save_fig_button.clicked.connect(self.save_fig)
 
     def fit(self, data_x, data_y):
         self.axes.plot(data_x, data_y, 'o')
 
         param, cov = curve_fit(self.__sigmoid, data_x, data_y, p0=(1.0, 55))
-        lin_x = np.arange(100, step=0.5)
+        lin_x = np.arange(120, step=0.5)
         self.axes.plot(lin_x, self.__sigmoid(lin_x, *param))
-        self.axes.set_title("Calculated Tm is %2.1f C" % param[1])
+        self.axes.set_title("Calculated Tm: %2.1f C" % param[1])
         self.axes.set_ylim(0, 1)
         self.canvas.draw()
+
+    def save_fig(self):
+        default_filename = "fsects_fitcurve.png"
+        filename = QtWidgets.QFileDialog.getSaveFileName(
+            self, "Save file", os.path.expanduser("~") + "/" + default_filename,
+            filter="images (*.png *.jpg *.pdf)")
+        file_save_to = filename[0]
+        if not file_save_to:
+            return
+        self.fig.savefig(file_save_to, bbox_inches='tight')
 
     # private
 
